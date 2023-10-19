861f2478464d5b9a94d98cdcf2c64878446e631d
HaykelBargouguy <haykel.bargougui@insat.ucar.tn>
2023-10-19 16:17:02 +0100

    last change

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { AddTodoDto } from './dto/Addtodo.dto';
 import { UpdateTodoDto } from './dto/Updatetodo.dto';
+import { TodoStatusEnum } from './entities/status.enum';
+import { TodoEntity } from './entities/todo.entity';
 
 @Controller('todo')
 export class TodoController {
@@ -11,7 +13,7 @@ export class TodoController {
 
 @Post()
 addTodo(@Body() body :AddTodoDto){
-    return  this.todoService.addTodoV2(body)
+    return  this.todoService.addTodoV1(body)
 }
 
 @Put(':id')
@@ -19,4 +21,55 @@ modifyTodo(@Body() body :Partial<UpdateTodoDto> ,@Param('id') id : number){
     
     return this.todoService.modifyTodo(id , body)
 }
+@Delete(':id')
+deleteTodo(@Param('id') id ){
+    return this.todoService.deleteTodo(id)
 }
+
+@Delete('soft/:id')
+SoftdeleteTodo(@Param('id') id ){
+    return this.todoService.SoftdeleteTodo(id)
+}
+
+@Delete('/restore/:id')
+RestoredeletedTodo(@Param ('id') id){
+    return this.todoService.RestoredeletedTodo(id)
+}
+
+@Get('count')
+async getTodosCountByStatus(): Promise<{ [key in TodoStatusEnum]: number }> {
+  return this.todoService.countTodosByStatus();
+}
+
+@Get('findAll')
+  async findAll(): Promise<TodoEntity[]> {
+    return this.todoService.findAllTodos();
+
+}
+
+  @Get()
+  async findTodos(
+    @Query('name') name?: string, 
+    @Query('description') description?: string,
+    @Query('status') status?: TodoStatusEnum
+  ): Promise<TodoEntity[]> {
+    return this.todoService.findTodosByCriteria(name, description, status);
+  }
+  @Get('pagination')
+  async findTodosByPage(
+    @Query('name') name?: string, 
+    @Query('description') description?: string,
+    @Query('status') status?: TodoStatusEnum,
+    @Query('limit') limit?: number,
+    @Query('offset') offset?: number
+  ): Promise<TodoEntity[]> {
+    return this.todoService.findTodosPaginated(name, description, status, limit, offset);
+  }
+
+
+
+
+}
+
+
+
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,56 +1,154 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { TodoEntity } from './entities/todo.entity';
 import { AddTodoDto } from './dto/Addtodo.dto';
 import { UpdateTodoDto } from './dto/Updatetodo.dto';
-
+import { TodoStatusEnum } from './entities/status.enum';
 @Injectable()
 export class TodoService {
+  sectionRepository: any;
   constructor(
     @InjectRepository(TodoEntity)
     private readonly todoRepository: Repository<TodoEntity>,
-  ) {}
+  ) { }
 
 
-  async modifyTodo(id : number, todo: Partial<UpdateTodoDto>): Promise<TodoEntity> {
-    const EntityToModify = await this.todoRepository.findOne({where : {id}})
-    
+  async modifyTodo(id: number, todo: Partial<UpdateTodoDto>): Promise<TodoEntity> {
+    const EntityToModify = await this.todoRepository.findOne({ where: { id } })
+
     if (!EntityToModify) {
       throw new Error('Todo is not found');
-  }
+    }
 
-  if (todo.name) {
+    if (todo.name) {
       EntityToModify.name = todo.name;
-  }
-  if (todo.description) {
+    }
+    if (todo.description) {
       EntityToModify.description = todo.description;
-  }
-  if (todo.status) {
+    }
+    if (todo.status) {
       EntityToModify.status = todo.status;
+    }
+
+    return await this.todoRepository.save(EntityToModify);
+
   }
-
-  return await this.todoRepository.save(EntityToModify);
-
-}
-
-      
-  
 
   // Version 1 : Utilisation directe de save
-  /*async addTodoV1(newtodo :AddTodoDto): Promise<TodoEntity> {
+  async addTodoV1(newtodo: AddTodoDto) {
     const todo = this.todoRepository.create(newtodo);
-    return await this.todoRepository.save(newtodo);
-  }*/
-
-  // Version 2 : Création manuelle puis utilisation de save
-    async addTodoV2(todo:AddTodoDto): Promise<TodoEntity> {
-    const name = todo.name;
-    const description = todo.description;
-    const newtodo = this.todoRepository.create({name, description})
     return await this.todoRepository.save(newtodo);
   }
 
+  // Version 2 : Création manuelle puis utilisation de save
+  /* async addTodoV2(todo:AddTodoDto): Promise<TodoEntity> {
+   const name = todo.name;
+   const description = todo.description;
+   const newtodo = this.todoRepository.create({name, description})
+   return await this.todoRepository.save(newtodo);*/
 
 
+
+  async deleteTodo(id):Promise<DeleteResult> {
+      return await this.todoRepository.delete(id);
+      } 
+  
+    
+    /*const EntityToDelete = await this.todoRepository.findOne({ where: { id } })
+
+    if (!EntityToDelete) {
+      throw new Error('Todo is not found');
+    }
+    await this.todoRepository.delete(EntityToDelete);
+    return `the entity with the id ${id} is deleted successfuly `
+  }*/
+
+  async SoftdeleteTodo(id: number): Promise<UpdateResult> {
+      return await this.todoRepository.softDelete(id);
+      }
+
+  async RestoredeletedTodo(id: number) {
+      return await this.todoRepository.restore(id);
+       
+    /*const EntityToDelete = await this.todoRepository.findOne({ where: { id } })
+
+    if (!EntityToDelete) {
+      throw new Error('Todo is not found');
+    }
+    await this.todoRepository.(EntityToDelete);
+    return `the entity with the id ${id} is deleted successfuly `*/
+  }
+
+  async countTodosByStatus(): Promise<{ [key in TodoStatusEnum]: number }> {
+    const counts: { [key in TodoStatusEnum]: number } = {
+      TODO: await this.todoRepository.count({ where: { status: TodoStatusEnum.TODO } }),
+      IN_PROGRESS: await this.todoRepository.count({ where: { status: TodoStatusEnum.IN_PROGRESS } }),
+      DONE: await this.todoRepository.count({ where: { status: TodoStatusEnum.DONE } }),
+    };
+    return counts;
+  }
+
+  async findAllTodos(): Promise<TodoEntity[]> {
+    return await this.todoRepository.find();
+  }
+// src/todo/todo.service.ts
+
+async findTodosByCriteria(name?: string, description?: string, status?: TodoStatusEnum): Promise<TodoEntity[]> {
+  const query = this.todoRepository.createQueryBuilder('todo');
+
+  if (name) {
+    query.andWhere('todo.name LIKE :name', { name: `%${name}%` });
+  }
+
+  if (description) {
+    query.andWhere('todo.description LIKE :description', { description: `%${description}%` });
+  }
+
+  if (status) {
+    query.andWhere('todo.status = :status', { status });
+  }
+
+  return await query.getMany();
+}
+// src/todo/todo.service.ts
+
+async findTodosPaginated(
+  name?: string,
+  description?: string,
+  status?: TodoStatusEnum,
+  limit?: number,
+  offset?: number
+): Promise<TodoEntity[]> {
+  const query = this.todoRepository.createQueryBuilder('todo');
+
+  if (name) {
+    query.andWhere('todo.name LIKE :name', { name: `%${name}%` });
+  }
+
+  if (description) {
+    query.andWhere('todo.description LIKE :description', { description: `%${description}%` });
+  }
+
+  if (status) {
+    query.andWhere('todo.status = :status', { status });
+  }
+
+  if (limit) {
+    query.limit(limit);
+  }
+
+  if (offset) {
+    query.offset(offset);
+  }
+
+  return await query.getMany();
+}
+
+
+
+
+
+  
+  
 }
